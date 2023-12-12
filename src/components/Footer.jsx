8080833d866490f07a40aa5bcb39d9afa922add9
Samouil <samouilpratziotis@gmail.com>
2023-12-12 19:47:25 +0200

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { logo } from '../assets'
+
+const Footer = () => {
+    return (
+      <footer className='w-full flex justify-center items-center flex-col'>
+  
+        <h2>
+          Created by Samouil Pratzioti
+        </h2>
+  
+      </footer>
+    )
+  }
+  
+  export default Footer
